5c8d95d419d5aa21467435aa4882e8702f451025
Mario Hernandez <mariohernandez@Marios-MacBook-Pro.local>
2022-06-29 20:23:37 -0700

    first commit

---
+++ client/movieflick/src/App/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+
+
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello THis is the App</h1>
+    </div>
+  );
+}
+
---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+//this initizialize the app
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+///THIS ONE CONNECTS TO MONGODB DATABASE
+const CONNECTION_URL =
+  "mongodb+srv://maarioherns21:94621Mar@cluster0.h9zud.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+////this allows  to connect to  the server on port 3001
+const port = process.env.PORT || 3001;
+
+app.listen(port, function () {
+  console.log(`Express app listening on port ${port}`);
+});
+
+mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+///THIS THEN WILL CONSOLE.LOG() WHERRE THE PORT IS RUNNING WHICH IS ON PORT 5000
+const db = mongoose.connection;
+
+// database connection event
+db.on("connected", function () {
+  console.log(`Mongoose connected to: ${db.host}:${db.port}`);
+});
+
+// ////THIS WILL MAKE SURE THERE IS NO WORNINGS
+// mongoose.set('useFindAndModify', false);
